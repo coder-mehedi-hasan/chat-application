a7608e0716a5d4676ecff771468c2930e7f1d183
Md Mehedi Hasan <87407354+coder-mehedi-hasan@users.noreply.github.com>
2023-10-03 09:17:03 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["cloudflare-ipfs.com"],
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-};
 
-module.exports = nextConfig;
