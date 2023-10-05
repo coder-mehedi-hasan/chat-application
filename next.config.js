6c2574d042161a2566e350fbc429045e3d8e5e01
Md Mehedi Hasan <mh8399807@gmail.com>
2023-10-05 12:51:46 +0600

    config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      domains: ["cloudflare-ipfs.com"],
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    typescript: {
+      // !! WARN !!
+      // Dangerously allow production builds to successfully complete even if
+      // your project has type errors.
+      // !! WARN !!
+      ignoreBuildErrors: true,
+    },
+  };
+  
+  module.exports = nextConfig;
+  
--- pages/chat.tsx
+++ pages/chat.tsx
@@ -6,7 +6,7 @@ export default function Chat() {
 
     return (
         <>
-            < Dashboard />
+            <Dashboard />
         </>
     )
 }
