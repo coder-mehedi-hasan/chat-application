80bd120c9643a38e63d2b0ada1daba88b67fb260
Md Mehedi Hasan <mh8399807@gmail.com>
2023-10-23 01:23:41 +0600

    responsive

--- components/common/HomeChat.tsx
+++ components/common/HomeChat.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function HomeChat() {
   return (
-    <div>HomeChatWeb</div>
+    <div style={{ height: "100vh",background:"rgb(0,0,0,0.3)" }} className='d-flex align-items-center justify-content-center'>
+      <h3 className='m-0 p-0 text-white fw-medium'>Wellcome to Chatting Application</h3>
+    </div>
   )
 }
