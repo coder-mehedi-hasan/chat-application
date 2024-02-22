bbb543d66d5932273b45a5a421bca505626fc3bf
coder-mehedi-hasan <mh8399807@gmail.com>
2024-02-22 08:26:35 +0600

    solving last issues

--- components/Messages/VoiceContent.tsx
+++ components/Messages/VoiceContent.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactAudioPlayer from 'react-audio-player';
+import { useStateProvider } from '../../context/StateContext';
 
 const VoiceContent = ({ message }) => {
     const url = message?.messageFiles?.length && message?.messageFiles[0]?.filepath;
-   // console.log("voice url",url)
+    const [{ userInfo }, dispatch]: any = useStateProvider()
+    const isSender = message?.messageFromUserID === userInfo?.id
     return (
         <>
             <ReactAudioPlayer
                 src={url}
                 controls
-                
+                className={isSender ? "sender-voice-content" : ""}
             />
         </>
     );
